b16f2fda0acd49d580905cf94212059cc8ea9c20
Rabbitwhite <34395602+whitedodo@users.noreply.github.com>
2020-10-09 15:35:11 +0900

    Update LogAdvisor.java

--- src/main/java/com/website/example/aop/LogAdvisor.java
+++ src/main/java/com/website/example/aop/LogAdvisor.java
@@ -1,3 +1,13 @@
+/*
+ * 	
+ *  주제(Subject): Spring Framework 4.2 - AOP 구현하기(Java - Context)
+ *  생성일자(Create Date): 2020-10-09
+ *  파일명(Filename): LogAdvisor.java
+ *  저자(Author): Dodo / rabbit.white at daum dot net
+ *  비고(Description):
+ * 
+ */
+
 package com.website.example.aop;
 
 import org.aspectj.lang.annotation.Aspect;
